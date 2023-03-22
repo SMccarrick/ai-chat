fadc31be5f1e7c5523738c2bd504fbf77dc35181
SMccarrick <smccarrick2804@gmail.com>
2023-03-22 21:29:15 +0000

    fix a few small issues with the button component

--- src/components/button/button.css.ts
+++ src/components/button/button.css.ts
@@ -1,17 +1,17 @@
 import { vars } from "$lib/styles.css";
 import { style } from "@vanilla-extract/css";
 
-// Define your styles using Vanilla Extract
 export const button = style({
   ...vars.tokens.textStyles.p,
   backgroundColor: vars.colors.uiElement.background,
   color: vars.colors.text,
-  padding: '8px 16px',
+  padding: vars.tokens.spacing.xs,
+  width: "100%",
   height: "4.8rem",
-  borderRadius: '4px',
-  border: "solid 1px",
-  borderColor: vars.colors.uiElement.border,
+  borderRadius: "0.8rem",
+  border: "none",
   cursor: 'pointer',
+  whiteSpace: "nowrap",
   ':hover': {
     backgroundColor: vars.colors.uiElement.backgroundHover,
     opacity: 0.8,
